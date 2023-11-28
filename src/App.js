4b545769afedb046b1823eb666a6235f11ca3183
Edier <gomezediercamilo@gmail.com>
2023-11-28 17:13:43 -0500

    terminacion de la parte explicativa de el instructor

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import Message from './message';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <h1>hola desde react</h1>
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>hola desde reacy</h1>
+      <Message name={'edier'}/>
     </div>
   );
 }
---
+++ src/message.js
@@ -0,0 +1,8 @@
+const Message = ({name}) => {
+    console.log(name);
+    return (
+        <h2>Este mensaje es el mensaje componente {name}</h2>
+    )
+}
+
+export default Message;
